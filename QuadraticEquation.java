abd1b433120e64a3ee999466d660fa71cc9d32c3
tuanpmt38 <37221796+tuanpmt38@users.noreply.github.com>
2018-04-01 21:11:18 +0700

    pf-java oop Quadratic Equation

--- QuadraticEquation.java
+++ QuadraticEquation.java
@@ -1,6 +1,59 @@
 public class QuadraticEquation {
+    private double a, b, c;
+    private double delta = 0;
 
-    public QuadraticEquation(){
-
+    public QuadraticEquation() {
     }
+
+    public QuadraticEquation(double a, double b, double c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public String getInfomation() {
+        return "Display {" + "a= " + a + ", b= " + b + ", c= " + c + '}';
+    }
+
+    public double getDiscriminant() {
+        delta = Math.pow(b, 2) - 4 * a * c;
+        return delta;
+    }
+
+    public double getRoot1() {
+        double sqrDelta = Math.sqrt(delta);
+        double r1 = (-b + sqrDelta) / (2 * a);
+        return r1;
+    }
+
+    public double getRoot2() {
+        double sqrDelta = Math.sqrt(delta);
+        double r2 = (-b - sqrDelta) / (2 * a);
+        return r2;
+    }
+
 }
--- QuadraticEquationTest.java
+++ QuadraticEquationTest.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class QuadraticEquationTest {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input a: ");
+        double number_a = scanner.nextDouble();
+        System.out.println("Input b: ");
+        double number_b = scanner.nextDouble();
+        System.out.println("Input c: ");
+        double number_c = scanner.nextDouble();
+        QuadraticEquation quadraticEquation = new QuadraticEquation(number_a, number_b, number_c);
+        System.out.println(quadraticEquation.getInfomation());
+        double delta = quadraticEquation.getDiscriminant();
+        double root1 = quadraticEquation.getRoot1();
+        double root2 = quadraticEquation.getRoot2();
+        if (delta > 0) {
+            System.out.println("The equation has two roots:" + root1+ "\t and "+root2);
+        } else if (delta == 0) {
+            System.out.println("QuadraticEquation has one result: " + root1);
+        } else {
+            System.out.println("The equation has no roots");
+        }
     }
+
+
 }
